6b0aaf8d7fab87a517a26e0685ccb6661839e20a
Boban BMW <sceka.shad0w@gmail.com>
2020-11-11 03:03:22 +0100

    find best matching single file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module autotorrent
 
 go 1.15
 
-require github.com/jackpal/bencode-go v1.0.0
+require (
+	github.com/agnivade/levenshtein v1.1.0
+	github.com/jackpal/bencode-go v1.0.0
+)
--- singleFile.go
+++ singleFile.go
@@ -1,12 +1,57 @@
 package main
 
+import (
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/agnivade/levenshtein"
+)
+
 func handleSingleFileTorrent(torrent *singleFileTorrent, downloads []node, links string) {
 	torrentSize := torrent.Info.Length
 
+	matchingFiles := make([]node, 0)
+
 	for _, file := range downloads {
 		if file.info.Size() == torrentSize {
-			// create link
-			// send to dl client
+			matchingFiles = append(matchingFiles, file)
 		}
 	}
+
+	var matchingFile node
+
+	if len(matchingFiles) == 0 {
+		return
+	} else if len(matchingFiles) == 1 {
+		matchingFile = matchingFiles[0]
+	} else {
+		matchingFile = getBestMatchingFile(matchingFiles, torrent.Info.Name)
+	}
+
+	err := os.Symlink(matchingFile.path, filepath.Join(links, torrent.Info.Name))
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		log.Println("Error linking", matchingFile.path, "->", torrent.Info.Name, err)
+		return
+	}
+	// send to dl client
+
+	log.Println(matchingFile)
+}
+
+func getBestMatchingFile(matchingFiles []node, torrentFileName string) node {
+	index := 0
+	minDistance := 500
+
+	for i, file := range matchingFiles {
+		distance := levenshtein.ComputeDistance(file.info.Name(), torrentFileName)
+
+		if distance < minDistance {
+			index = i
+			minDistance = distance
+		}
+	}
+
+	return matchingFiles[index]
 }
